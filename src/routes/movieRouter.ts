694b3e524960122e663f9a60bd908160c0a56af7
Ice Chain <afolabiyinka738@gmail.com>
2026-04-28 13:46:44 +0100

    feat: add route to check if a movie is in favourites

--- src/routes/movieRouter.ts
+++ src/routes/movieRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { addToFavorites, fetchFavourites, removeFromFavourites } from "../controllers/favouritesController";
+import { addToFavorites, fetchFavourites, inFavourites, removeFromFavourites } from "../controllers/favouritesController";
 
 export const MovieRouter = Router()
 
 MovieRouter.route("/").get(fetchFavourites)
 MovieRouter.route("/add").post(addToFavorites)
-MovieRouter.route("/inFavourite").get(addToFavorites)
+MovieRouter.route("/inFavourites").get(inFavourites)
 MovieRouter.route("/remove").delete(removeFromFavourites)
