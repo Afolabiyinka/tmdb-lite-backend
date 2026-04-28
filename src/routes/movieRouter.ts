a7c19bd88c524e1f3db50a5f7bbcc329099f3624
Ice Chain <afolabiyinka738@gmail.com>
2026-04-28 13:59:55 +0100

    fix: update inFavourites route to use movieId instead of id

--- src/routes/movieRouter.ts
+++ src/routes/movieRouter.ts
@@ -5,5 +5,5 @@ export const MovieRouter = Router()
 
 MovieRouter.route("/").get(fetchFavourites)
 MovieRouter.route("/add").post(addToFavorites)
-MovieRouter.route("/inFavourites/:id").get(inFavourites)
+MovieRouter.route("/inFavourites/:movieId").get(inFavourites)
 MovieRouter.route("/remove").delete(removeFromFavourites)
