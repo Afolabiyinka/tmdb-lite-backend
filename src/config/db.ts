014f8741f5b58f4d1219e5d3f741ad6d5d1046be
Ice Chain <afolabiyinka738@gmail.com>
2026-07-03 08:41:07 +0100

    refactor: clean up sequelize initialization and comment out local database connection code

--- src/config/db.ts
+++ src/config/db.ts
@@ -9,31 +9,31 @@ if (!DATABASE_URL) {
     throw new Error("DATABASE_URL environment variable is not defined");
 }
 
-// export const sequelize = new Sequelize(DATABASE_URL, {
-//     dialect: "postgres",
-//     logging: false,
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//         },
-//     },
-// });
+export const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
 
 
 
 // Local Database Connections
-export const sequelize = new Sequelize(
-    process.env.DATABASE_NAME as string,
-    process.env.DATABASE_USERNAME as string,
-    process.env.DATABASE_PASSWORD,
-    {
-        port: Number(process.env.DATABASE_HOST) || 5432,
-        host: process.env.DATABASE_HOST || "localhost",
-        dialect: "postgres",
-        logging: false,
-    }
-);
+// export const sequelize = new Sequelize(
+//     process.env.DATABASE_NAME as string,
+//     process.env.DATABASE_USERNAME as string,
+//     process.env.DATABASE_PASSWORD,
+//     {
+//         port: Number(process.env.DATABASE_HOST) || 5432,
+//         host: process.env.DATABASE_HOST || "localhost",
+//         dialect: "postgres",
+//         logging: false,
+//     }
+// );
 
 export const connectDb = async () => {
     try {
